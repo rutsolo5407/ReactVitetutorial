c0c660d09175388cc2535e26b3d8f9e70a9be2ae
waroot boonmarkmee <waroot.b@egat.co.th>
2024-03-27 13:27:48 +0700

    first commit

---
+++ App.tsx
@@ -0,0 +1,4 @@
+// Import styles of packages that you've installed.
+// All packages except `@mantine/hooks` require styles imports
+import '@mantine/core/styles.css';
+import { MantineProvider } from '@mantine/core';
